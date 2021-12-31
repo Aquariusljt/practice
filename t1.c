a0d699db5936f7b230bb897e73afae99a3391e5c
Aquariusljt <96865002+Aquariusljt@users.noreply.github.com>
2021-12-31 17:17:32 +0800

    Add files via upload

---
+++ t1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define PRICE 10
+
+int main(void)
+{
+	int x = 0;
+	int a = 0;
+	int b = 0;
+	a = 333;
+	b = 555;
+	x = 333 * 555;
+	printf("%d\n", x);
+	return 0;
+ //   float length = 0;
+	//float width = 0;
+	//float height = 0;
+	//float volume = 0;
+	//printf("please input length,width,height\n");
+	//printf("please input length:");
+	//scanf_s("%f",&length);
+	//printf("please input width:");
+	//scanf_s("%f", &width);
+	//printf("please input height:");
+	//scanf_s("%f", &height);
+	//volume = length * width * height;
+	//printf("volume:%f\n", volume);
+	//return 0;
+}
+
